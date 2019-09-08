9145749388c2ed485f09ff25551e68c8f71262bc
github username <github email address>
2019-09-08 01:30:58 -0400

    Done.

---
+++ app/serializers/sighting_serializer.rb
@@ -0,0 +1,16 @@
+# class SightingSerializer
+#   include FastJsonapi::ObjectSerializer
+#   attributes :created_at
+# end
+
+# class SightingSerializer
+#   include FastJsonapi::ObjectSerializer
+#   attributes :created_at, :bird, :location
+# end
+
+class SightingSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :created_at
+  belongs_to :bird
+  belongs_to :location
+end
